c7d4575c010ccf24e74617fa391f96accc1ff3a0
sramador45 <58308854+sramador45@users.noreply.github.com>
2022-07-26 01:55:55 -0600

    cambios
    cambios

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 
-    /*
-  esta funcion es para recibir el parametro que es el id del elemento 
-  const list repos arrow function para poder acceder accesar a un request de  la api  de github 
-  */
-const button = document.getElementById('buscar');
-const listRepos = username => {
-    //va a llamar const repos cuando este listo
-    const repos= await fetch(`https://api.github.com/users/${username}/repos?type=owner&sort=updated`) .then(res => res.json());
-    const content=document.getElementById("contenido");
-    const markup =listRepos.map((repo)=>`<li>
-    <a href="${repo.html_url}">${repo.name}</a>
-    (⭐️ ${repo.stargazers_count})
-    </li>`)
-
- //sirve para concatenar todos los elementos del arreglo en un string
-    content.innerHTML=`${markup.join((''))}`
+ const button = document.getElementById('buscar');
 
 
-};
-
-//evento para cuando se haga click
-button.onclick=function(){
-    const nameRepo=document.getElementById("name").value;
-    listRepos(nameRepo)
-}
-
+ const listRepos = async username => {
+   const repos = await fetch(
+     `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+   ).then(res => res.json());
+ 
+   const content = document.getElementById('contenido');
+   const markup = repos.map(
+     repo => `
+     <li>
+       <a href="${repo.html_url}">${repo.name}</a> 
+       (⭐️ ${repo.stargazers_count})
+     </li>
+   `
+   );
+ 
+   content.innerHTML = `<ul>${markup.join('')}</ul>`;
+ };
+ 
+ 
+ button.onclick = function() {
+   const nameRepo = document.getElementById('name').value;
+     listRepos(nameRepo);
+ }
