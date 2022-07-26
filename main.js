66370e2f12aece207750bedcc15c81d1163d570a
sramador45 <58308854+sramador45@users.noreply.github.com>
2022-07-26 01:16:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+    /*
+  esta funcion es para recibir el parametro que es el id del elemento 
+  const list repos arrow function para poder acceder accesar a un request de  la api  de github 
+  */
+const button = document.getElementById('buscar');
+const listRepos = username => {
+    //va a llamar const repos cuando este listo
+    const repos= await fetch(`https://api.github.com/users/${username}/repos?type=owner&sort=updated`) .then(res => res.json());
+    const content=document.getElementById("contenido");
+    const markup =listRepos.map((repo)=>`<li>
+    <a href="${repo.html_url}">${repo.name}</a>
+    </li>`)
+
+ //sirve para concatenar todos los elementos del arreglo en un string
+    content.innerHTML=`${markup.join((''))}`
+
+
+};
+
+//evento para cuando se haga click
+button.onclick=function(){
+    const nameRepo=document.getElementById("name").value;
+    listRepos(nameRepo)
+}
+
